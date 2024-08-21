00d4f93c00157bb8f98e83f914e922efbcc19cfe
anabelendomenech <domenechanaa@gmail.com>
2024-08-21 20:57:27 -0300

    Update products.js

---
+++ js/products.js
@@ -0,0 +1,42 @@
+const Autos_101 = https://japceibal.github.io/emercado-api/cats_products/101.json
+
+let showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+let hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+let getJSONData = function(url){
+    let result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner();
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(CATEGORIES_URL).then(function(resultado){
+        if(resultado.status = 'ok'){
+            console.log(resultado.data);
+        }
+    })
+})
