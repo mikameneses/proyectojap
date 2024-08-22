f6bea2afeb383fdb0517a05219cfb8fee3ce145c
anabelendomenech <domenechanaa@gmail.com>
2024-08-22 20:01:05 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,8 +1,8 @@
 const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"; 
        document.addEventListener("DOMContentLoaded", function(){
-           getJSONData(url).then(function(resultado){
-               if(resultado.status === 'ok'){
-                   console.log(resultado.data);
+           getJSONData(url).then(function(result){
+               if(result.status === 'ok'){
+                   console.log(result.data);
                }
            })
        });
