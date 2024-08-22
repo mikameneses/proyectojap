63f2683e495b265c5db36203eff4f0cea0a45e35
anabelendomenech <domenechanaa@gmail.com>
2024-08-22 18:48:34 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,34 +1,34 @@
-// const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"; //
-
-let getJSONData = function(url){
-    let result = {};
-   
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        
-        return result;
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(url).then(function(result){
-        if(result.status === 'ok'){
-            console.log(result.data);
-        }
-    })
-})
+const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"; //
+let fetchJSONData = function(url){
+           let result = {};
+    
+           return fetch(url)
+           .then(response => {
+             if (response.ok) {
+               return response.json();
+             } else {
+               throw Error(response.statusText);
+             }
+           })
+           .then(function(response) {
+               result.status = 'ok';
+               result.data = response;
+               
+               return result;
+           })
+           .catch(function(error) {
+               result.status = 'error';
+               result.data = error;
+               
+               return result;
+           });
+       }
+    
+       document.addEventListener("DOMContentLoaded", function(){
+           fetchJSONData(url).then(function(result){
+               if(result.status === 'ok'){
+                   console.log(result.data);
+               }
+           });
+       });
+   })();
