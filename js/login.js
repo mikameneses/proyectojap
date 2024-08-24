3d10d0bb291f3cd36ea87d3d48094a3b374acc4b
anabelendomenech <domenechanaa@gmail.com>
2024-08-24 17:34:54 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,15 +7,12 @@
            
 
 document.addEventListener('DOMContentLoaded', function() {
-   
+
     const loginButton = document.getElementById('loginButton');
-   
+
+
     loginButton.onclick = function() {
-      
+
         window.location.href = 'index.html';
-    };
-});
-
-
 
 
