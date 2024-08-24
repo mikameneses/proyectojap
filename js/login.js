e694c6ed8479b1568ac7c121dd197f820f449e68
anabelendomenech <domenechanaa@gmail.com>
2024-08-24 17:29:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
- function validateLogin() {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        if (username === "" || password === "") {
-            alert("Por favor, complete todos los campos.");
+ //function validateLogin() {
+ //       var username = document.getElementById("username").value;
+ //       var password = document.getElementById("password").value;
+  //      if (username === "" || password === "") {
+  //          alert("Por favor, complete todos los campos.");
         }  } 
            
 
 document.addEventListener('DOMContentLoaded', function() {
    
     const loginButton = document.getElementById('loginButton');
-   function validateLogin();
+   
     loginButton.onclick = function() {
       
         window.location.href = 'index.html';
@@ -19,24 +19,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-/*document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.getElementById('loginButton');
-
-    loginButton.onclick = function() {
-        // Simula autenticación exitosa (esto debería cambiar con la validación real)
-        const validUser = true; // Aquí deberías verificar las credenciales del usuario
-
-        if (validUser) {
-            // Guarda el estado de sesión en localStorage
-            localStorage.setItem('loggedIn', 'true');
-
-            // Redirige al usuario a 'index.html'
-            window.location.href = 'index.html';
-        } else {
-            alert('Credenciales inválidas. Por favor, inténtelo de nuevo.');
-        }
-    };
-});
-
-
-    
