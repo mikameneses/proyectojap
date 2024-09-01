1b527e53a638c72961ce482429f3d7f871b40c08
anabelendomenech <domenechanaa@gmail.com>
2024-09-01 16:34:33 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,14 @@
-
 function validateLogin() {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-        // Validar que los campos no estén vacíos
-        if (username.trim() !== "" && password.trim() !== "") {
-            window.location.href = "index.html";
-        } else {
-            alert("Por favor, complete todos los campos.");
-        }
+    // Validar que los campos no estén vacíos
+    if (username.trim() !== "" && password.trim() !== "") {
+        // Guardar el nombre de usuario en el almacenamiento local
+        localStorage.setItem("username", username);
+
+        window.location.href = "index.html";
+    } else {
+        alert("Por favor, complete todos los campos.");
     }
+}
