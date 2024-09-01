bb89f96d5967e0db0ae25975387b2daeebcdc22c
helenaabella127 <sofiAbella10@gmail.com>
2024-09-01 18:03:31 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,9 @@ function validateLogin() {
 
         // Validar que los campos no estén vacíos
         if (username.trim() !== "" && password.trim() !== "") {
+                sessionStorage.setItem("loggedIn", "yes")
+               localStorage.setItem("username", username)
+               localStorage.setItem("password", password)
             window.location.href = "index.html";
         } else {
             alert("Por favor, complete todos los campos.");
