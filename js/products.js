826dfd04060f37a76160fff7944fb3293b510797
anabelendomenech <domenechanaa@gmail.com>
2024-09-14 12:58:05 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     // Recupera el catID del almacenamiento local
     const categoriaId = localStorage.getItem("catID"); 
-    const url =  'https://japceibal.github.io/emercado-api/products/${categoriaId}.json' ⁠;
+    const url =  'https://japceibal.github.io/emercado-api/cats_products/${categoriaId}.json' ⁠;
 
     let products = []; // Variable global para almacenar los productos
 
