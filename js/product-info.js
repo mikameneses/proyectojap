3e54db8dea98857ca06d15c2d0eb384f8ddf5a41
smartinrr <sabrinamartinrey@gmail.com>
2024-09-26 21:16:28 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -69,4 +69,20 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.error('Producto no encontrado en localStorage');
     }
+
+//Solicitud pitar-despintar estrellas
+     const stars = document.querySelectorAll(".star");
+
+    stars.forEach(function(star, index) {
+        star.addEventListener("click", function() {
+            for (let i=0; i<=index; i++) {
+                stars[i].classList.add("checked");
+            }
+            for (let i=index+1; i<stars.length; i++) {
+                stars[i].classList.remove("checked");
+            }
+    })
+})
+
 });
+
