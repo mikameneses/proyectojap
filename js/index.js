35f1e600e95ea740a407de7c7b84125f3ecaa74a
anabelendomenech <domenechanaa@gmail.com>
2024-10-10 20:05:52 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,20 +11,31 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
-    document.addEventListener('DOMContentLoaded', function() {
+    
     // Verifica si el usuario está autenticado
     const loggedIn = localStorage.getItem('loggedIn');
-
     if (!loggedIn) {
-        // Redirige al usuario al login si no ha iniciado sesión
         window.location.href = 'login.html';
     }
-   });
-     window.onload = function() {
-        var username = localStorage.getItem("username");
-        if (username) {
-            document.getElementById("userDisplay").textContent = username;
-        }
-    };
 
+    // Muestra el nombre de usuario si está almacenado
+    var username = localStorage.getItem("username");
+    if (username) {
+        document.getElementById("userDisplay").textContent = username;
+    }
+
+    // Función de logout
+    function logout() {
+        // Elimina el token de autenticación del localStorage
+        localStorage.removeItem('authToken');
+        
+        // Redirecciona al usuario a la página de inicio de sesión
+        window.location.href = 'login.html';
+    }
+
+    // Agrega un evento clic al enlace de cerrar sesión
+    document.getElementById('logoutLink').addEventListener('click', function(event) {
+        event.preventDefault(); // Previene el comportamiento por defecto del enlace
+        logout();               // Llama a la función logout
+    });
 });
