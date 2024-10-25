5ba463e12541621f10090c800c19032ec284171e
anabelendomenech <domenechanaa@gmail.com>
2024-10-24 21:51:40 -0300

    Update cart.js

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+       const storedProduct = localStorage.getItem('selectedProduct');
+       
+       if (storedProduct) {
+           const product = JSON.parse(storedProduct);
+           // Logica aquí para mostrar el producto en el carrito
+           const cartContainer = document.getElementById('cart-container');
+           cartContainer.innerHTML = `
+               <div>
+                   <img src="${product.image}" alt="${product.name}" class="img-thumbnail" style="width: 100px;">
+                   <p>Nombre: ${product.name}</p>
+                   <p>Costo: ${product.cost} ${product.currency}</p>
+                   <p>Cantidad: <input type="number" value="${product.quantity}"></p>
+                   <p>Subtotal: ${product.cost * product.quantity} ${product.currency}</p>
+               </div>
+           `;
+       }
+   });
