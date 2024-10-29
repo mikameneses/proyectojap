181c84abc75b2ca9a6dd0b320feca79e46a5ae34
mikameneses <mikameneses99@gmail.com>
2024-10-28 21:36:45 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
        const storedProduct = localStorage.getItem('selectedProduct');
+
+       
        
        if (storedProduct) {
            const product = JSON.parse(storedProduct);
            // Logica aquí para mostrar el producto en el carrito
            const cartContainer = document.getElementById('cart-container');
+                document.getElementById('empty-cart-message').style.display = 'none';
            cartContainer.innerHTML = `
                <div>
                    <img src="${product.image}" alt="${product.name}" class="img-thumbnail" style="width: 100px;">
