8c8b77bb32e7dc0f2b3b19ae90fe33614cde05aa
anabelendomenech <domenechanaa@gmail.com>
2024-12-01 22:34:32 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-const CATEGORIES_URL = "http://localhost:3000/cats/cat.json";
+const CATEGORIES_URL = "http://localhost:3000/cats";
 const PUBLISH_PRODUCT_URL = "http://localhost:3000/sell/publish.json";
 const PRODUCTS_URL = "http://localhost:3000/cats_products/";
 const PRODUCT_INFO_URL = "http://localhost:3000/products/";
@@ -15,29 +15,43 @@ let hideSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
+let getJSONData = function(url, token = null) {
+  let result = {};
+  showSpinner();
+
+  // Configura los headers para la petición.
+  let headers = {
+    'Content-Type': 'application/json'
+  };
+
+  // Si se proporciona un token, lo agrega a los headers.
+  if (token) {
+      headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return fetch(url, {
+    method: 'GET',
+    headers: headers
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
         hideSpinner();
         return result;
-    });
+  })
+  .catch(function(error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
+  });
 }
 
 document.getElementById("logoutLink").addEventListener("click", function (event) {
