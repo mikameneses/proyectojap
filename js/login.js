f4e31c46b30c8298414c1aa36718837f33375071
anabelendomenech <domenechanaa@gmail.com>
2024-12-01 21:18:56 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,14 +2,36 @@ function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Validar que los campos no estén vacíos
-    if (username.trim() !== "" && password.trim() !== "") {
-        // Guardar el nombre de usuario en el almacenamiento local
-        localStorage.setItem("username", username);
+fetch('http://localhost:3000/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    username: username,
+    password: password
+  })
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Error al iniciar sesión'); 
+  }
+  return response.json();
+})
+.then(data => {
+  // Login exitoso: guardamos el token en localStorage
+  localStorage.setItem('token', data.token); 
 
-        window.location.href = "index.html";
-    } else {
-        alert("Por favor, complete todos los campos.");
-    }
+  // ... redirigir al usuario a la página principal
+    window.location.href = "index.html";
+     
+})
+.catch(error => {
+  // Manejar errores de login (mostrar mensaje al usuario, etc.)
+  console.error('Error:', error); 
+})
+
 }
 
+
+  
